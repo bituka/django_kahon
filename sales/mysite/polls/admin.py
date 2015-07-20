d5c1cc58d6ca4146e22e5259de009992ec9e77ba
stoned-code-warrior <goryo.webdev@gmail.com>
2015-07-20 22:17:08 +0000

    Initial commit

---
+++ sales/mysite/polls/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Choice, Question
+'''
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+'''
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
